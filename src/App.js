864e40989d4d82ffee671956f656e2cc2338c835
torik <riqibrekele@gmail.com>
2020-09-11 15:51:38 +0700

    assets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        todoList:[],
+        activeItem:{
+          id:null,
+          title:'',
+          completed:false,
+        },
+        editing:false
+      }
+      this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+  componentWillMount(){
+    this.fetchTasks()
+  }
+
+  fetchTasks(){
+    console.log('fetching...')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data =>
+      console.log('Data: ', data)
+    )
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <div id="task-container">
+          
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+
+                <div style={{ flex: 6}}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Add Task"/>
+                </div>
+
+                <div style={{ flex: 1}}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add"/>
+                </div>
+
+              </div>
+            </form>
+          </div>
+
+          <div id="list-wrapper">
+
+          </div>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
